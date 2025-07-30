5e05613475c95e39b1f144063e41f00d6907ea37
otizgit <emmanueljoe777@gmail.com>
2025-07-30 15:35:30 +0100

    swapping and loader fix

--- src/App.jsx
+++ src/App.jsx
@@ -263,24 +263,28 @@ function App() {
               Withdraw
             </button>
           </div>
-          {/* <div className="flex gap-2 mt-2 mb-1">
-              <Icon
-                icon="line-md:loading-alt-loop"
-                className="text-[1.3rem] text-red-500"
-              />
-              <p className="text-[0.8rem] text-red-400 font-semibold">
-                2/7 days of processing left
-              </p>
+          {isConnected && (
+            <div>
+              <div className="flex gap-2 mt-2 mb-1">
+                <Icon
+                  icon="line-md:loading-alt-loop"
+                  className="text-[1.3rem] text-red-500"
+                />
+                <p className="text-[0.8rem] text-red-400 font-semibold">
+                  2/7 days of processing left
+                </p>
+              </div>
+              <div className="flex gap-2 mt-1 mb-1">
+                <Icon
+                  icon="line-md:loading-alt-loop"
+                  className="text-[1.3rem] text-red-500"
+                />
+                <p className="text-[0.8rem] text-red-400 font-semibold">
+                  Swapping K to USDC
+                </p>
+              </div>
             </div>
-            <div className="flex gap-2 mt-1 mb-1">
-              <Icon
-                icon="line-md:loading-alt-loop"
-                className="text-[1.3rem] text-red-500"
-              />
-              <p className="text-[0.8rem] text-red-400 font-semibold">
-                Swapping K to USDC
-              </p>
-            </div> */}
+          )}
           <p className="text-[0.8rem] text-center text-gray-500 mt-2 font-semibold">
             Withdrawal now takes 5-7 days due to verification process
           </p>
